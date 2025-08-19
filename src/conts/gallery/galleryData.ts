82e8234cc6af2d957db5a2d20d1776c2a1b89a9d
Bok <pc791@naver.com>
2025-08-19 17:11:14 +0900

    Delete src directory

--- src/conts/gallery/galleryData.ts
+++
@@ -1,7 +0,0 @@
-export interface GalleryItem {
-    id: number;
-    title: string;   
-    image : string;   
-}
-
-export const galleryList: GalleryItem[] = JSON.parse(localStorage.getItem('galleryList') || '[]');
