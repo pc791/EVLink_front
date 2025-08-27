46a0aa4b393610fae4efdf56e05714a2ceb66434
serelee08 <serelee08@gmail.com>
2025-08-27 18:08:58 +0900

    Add files via upload

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -5,15 +5,15 @@ import { useAuth } from '../auth/AuthProvider'
 
 
 // Page Components
-
 import EVhome from '../components/home/EVhome'
 import Login from '../components/login/Login';
 import FAQ from '../components/community/faq/FAQ'
 import Event from '../components/community/event/Event';
-import Map from '../components/map/Map'
+import Notice from '../components/community/notice/Notice';
+import NoticeDetail from '../components/community/notice/NoticeDetail';
 // import Map from '../components/map/Map';
 // import mypage from '.conts/mypage/Mypage';
-// import Notice from '../components/notice/Notice';
+
 
 // import Analysis from '../components/analysis/Analysis';
 // import Service from '../components/service/Service';
@@ -25,15 +25,15 @@ const AppRoutes: React.FC = () => {
     const routeList = [ 
         { path: '/', element: <EVhome />},
         { path: '/Login', element: <Login /> },// 로그인
-        { path: '/map', element: <Map />  }, // 충전소 찾기 (맵)
+        // { path: '/map', element: <Map />  }, // 충전소 찾기 (맵)
         // { path: '/mypage', element: <Mypage /> }, // 마이페이지
-        // { path: '/notice', element: <Notice /> }, // 공지사항
+        { path: '/notice', element: <Notice /> }, // 공지사항
+        { path: '/notice/:id', element: <NoticeDetail /> }, // 공지사항
         { path: '/faq', element: <FAQ /> }, // FAQ
         { path: '/event', element: <Event /> }, // 이벤트
         // { path: '/analysis', element: <Analysis /> }, // 통계
         // { path: '/service', element: <Service /> }, // 서비스안내
-        // { path: '/reserveform', element: <Reserveform />}, // 예약 폼
-        // { path: '/reservelist', element: <Reservelist />}, // 예약 리스트
+
     ]
     
     
